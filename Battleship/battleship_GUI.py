c9a560da309a544ec6420e9ed8266fa2e35c7d46
kevinjbrummer <88122724+kevinjbrummer@users.noreply.github.com>
2021-08-20 00:50:45 +0900

    Update battleship_GUI.py

--- Battleship/battleship_GUI.py
+++ Battleship/battleship_GUI.py
@@ -6,12 +6,11 @@ cell_size = 45
 remaining_ships = 5
 
 def setup():
-    global root, player_map, com_map, com_array, pmap_array
+    global root,com_map, com_array
     global status_label, win_label, frame, carrier_label
     global battleship_label, destroyer_label, submarine_label
-    global patrol_boat_label, pstatus_label, pcarrier_label, pbattleship_label
-    global pdestroyer_label, psubmarine_label, ppatrol_boat_label
-    
+    global patrol_boat_label
+
     root = Tk()
     root.title('Battleship')
     root.attributes('-fullscreen', True)
@@ -20,67 +19,47 @@ def setup():
     
 
 
-    com_array = battleship_game.main()
-    pmap_array = battleship_game.main()
-    
+    com_array = battleship_game.main()    
     
 
-    player_map = make_map()
-    make_grid(player_map)
-    set_ships()
-    player_map.bind('<Button-1>', pm_grid_handler)
     com_map = make_map()
     make_grid(com_map)
     com_map.bind('<Button-1>', com_grid_handler)
 
     status_label = Label(root, font=('', 35), text='', bg=bg_color)
     status_label.grid(row=0, column=2, rowspan=5, padx=10)
+
+    
     carrier_label = Label(root, font=('', 35), text='Carrier',
                           bg=bg_color)
     carrier_label.grid(row=0, column=0)
+    
     battleship_label = Label(root, font=('', 35), text='Battleship',
                              bg=bg_color)
     battleship_label.grid(row=1, column=0)
+    
     destroyer_label = Label(root, font=('', 35), text='Destroyer',
                             bg=bg_color)
     destroyer_label.grid(row=2, column=0)
+    
     submarine_label = Label(root, font=('', 35), text='Submarine',
                             bg=bg_color)
     submarine_label.grid(row=3, column=0)
+    
     patrol_boat_label = Label(root, font=('', 35), text='Patrol Boat',
                             bg=bg_color)
     patrol_boat_label.grid(row=4, column=0)
     
 
-    pstatus_label = Label(root, font=('', 35), text='', bg=bg_color)
-    pstatus_label.grid(row=6, column=2, rowspan=5, padx=10)
-    pcarrier_label = Label(root, font=('', 35), text='Carrier',
-                          bg=bg_color)
-    pcarrier_label.grid(row=6, column=0)
-    pbattleship_label = Label(root, font=('', 35), text='Battleship',
-                             bg=bg_color)
-    pbattleship_label.grid(row=7, column=0)
-    pdestroyer_label = Label(root, font=('', 35), text='Destroyer',
-                            bg=bg_color)
-    pdestroyer_label.grid(row=8, column=0)
-    psubmarine_label = Label(root, font=('', 35), text='Submarine',
-                            bg=bg_color)
-    psubmarine_label.grid(row=9, column=0)
-    ppatrol_boat_label = Label(root, font=('', 35), text='Patrol Boat',
-                            bg=bg_color)
-    ppatrol_boat_label.grid(row=10, column=0)
-
-    barrier_label = Label(root, text='-'*290, bg = 'black')
-    barrier_label.grid(row=5, columnspan=3)
-
-
     win_label = Label(root, font=('', 50), text='YOU WIN!', bg=bg_color)
 
     for col in range(0,3):
         root.grid_columnconfigure(col, minsize=450)
+    for row in range(0,5):
+        root.grid_rowconfigure(row, minsize = 200)
+        
 
-    com_map.grid(row=0, column=1, rowspan=5, padx=5, pady=5)
-    player_map.grid(row=6, column=1, rowspan=5, padx=5, pady=5)
+    com_map.grid(row=0, column=1, rowspan=10, padx=5, pady=5)
 
     root.bind('<Return>', stop)
 
@@ -99,13 +78,7 @@ def make_map():
                         borderwidth=0,
                         highlightthickness=0,
                         bg='dodgerblue')
-def pm_grid_handler(event):
-    global player_map
-    
-    x = int(event.x/cell_size)
-    y = int(event.y/cell_size)
-    
-    fill_square(x, y, 'black', player_map)
+
 
 def com_grid_handler(event):
     global com_map, remaining_ships
@@ -151,9 +124,6 @@ def com_grid_handler(event):
                 win()
         else:
             status_label.configure(text= 'Hit!')
-    x = random.randint(0, 9)
-    y = random.randint(0, 9)
-    make_x(x, y, 'black', player_map)
 
 def fill_square(row, column, color, grid):
     grid.create_rectangle(row*cell_size, column*cell_size,
@@ -198,11 +168,6 @@ def strike(text):
         result = result + c + '\u0336'
     return result
 
-def set_ships():
-    for i in range(0, 10):
-        for j in range(0, 10):
-            if pmap_array[i][j] != 0:
-                       fill_square(j, i, 'gray', player_map)
         
     
     
